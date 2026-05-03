ecb8d2dc9901e825adfe845dbe315a34f2711192
Andrii Pr <a.v.prykhodko25@gmail.com>
2026-05-03 18:18:14 +0200

    chore(settings): define media url

--- pet_shelter/settings.py
+++ pet_shelter/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
 LOGIN_URL = "/login"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
